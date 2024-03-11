85e5bc12671742bb314428981c588177d6076bc6
ofarbrot <oskarfar@stud.ntnu.no>
2024-03-11 21:59:34 +0100

    Update trainig_algorithm.py

--- prosjekt_2_utlevert_kode/trainig_algorithm.py
+++ prosjekt_2_utlevert_kode/trainig_algorithm.py
@@ -1,52 +1,52 @@
-'''
-import numpy as np
+def TrainingAlgorithmAdding(n_iter):
 
-from layers import Attention, Softmax, CrossEntropy, LinearLayer, Relu, EmbedPosition, FeedForward, UnembedPosition
-from neural_network import NeuralNetwork
-from data_generators import get_xy_sort
+    from data_generators import get_train_test_addition
+    from utils import onehot
+    from layers import LinearLayer,EmbedPosition,Attention,Softmax,CrossEntropy,FeedForward
+    from neural_network import NeuralNetwork
 
-def TrainingAlgorithm(n_iter, B, alpha):
-    NN = NeuralNetwork()
-    D = np.zeros(B)
+    #dimensjoner og størrelser til x og y
+    n_digits = 2
+    n_max = 3*n_digits
+    m = 10
+
+    #definerer størrelsen på parametermatrisene
+    d = 15
+    k = 5
+    p = 20
+
+    #henter treningsdata
+    data = get_train_test_addition(n_digits,samples_per_batch=100,n_batches_train=4)
+
+    x = data['x_train'][0]
+    X = onehot(x,m)
+    y = data['y_train'][0]
+
     
+    embed = EmbedPosition(n_max,m,d)
+    att1 = Attention(d,k)
+    ff1 = FeedForward(d,p)
+    un_embed = LinearLayer(d,m)
+    softmax = Softmax()
+    loss = CrossEntropy()
 
-    for j in range(1, n_iter+1):
-        for k in range(1, B+1):
-            x, y = D[k]
-            NN.forward(x)
-            NN.backward(y)
-            NN.step_adam(alpha)
+    #"manuelt" forward pass (tilsvarende algoritme 1)
+    z0 = embed.forward(X)
+    z1 = att1.forward(z0)
+    z2 = ff1.forward(z1)
+    z = un_embed.forward(z2)
+    Z = softmax.forward(z)
 
-    return NN #Får i teorien et neural network som er ferdigtrent
-'''
+    layers = [embed,att1,ff1,un_embed,softmax]
+    nn = NeuralNetwork(layers)
 
+    #forward pass tilsvarende algoritme 1
+    Z = nn.forward(X)
 
-from data_generators import get_train_test_addition
+    #beregner loss med CrossEntropy
+    L = loss.forward(Z,y)
+    print(L)
 
-#dimensjoner og størrelser til x og y
-n_digits = 2
-n_max = 3*n_digits
-m = 10
-
-#definerer størrelsen på parametermatrisene
-d = 15
-k = 5
-p = 20
-
-#henter treningsdata
-data = get_train_test_addition(n_digits,samples_per_batch=100,n_batches_train=4)
-
-from utils import onehot
-
-x = data['x_train'][0]
-X = onehot(x,m)
-y = data['y_train'][0]
-
-from layers import LinearLayer,EmbedPosition,Attention,Softmax,CrossEntropy,FeedForward
-
-embed = EmbedPosition(n_max,m,d)
-att1 = Attention(d,k)
-ff1 = FeedForward(d,p)
-un_embed = LinearLayer(d,m)
-softmax = Softmax()
-loss = CrossEntropy()
+    #backward pass tilsvarende algoritme 2
+    dLdZ = loss.backward()
+    nn.backward(dLdZ)
