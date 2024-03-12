dcf3ac4b04c0bec4b4677df5478c79583d898f24
imlovold <imlovold@stud.ntnu.no>
2024-03-12 13:46:19 +0100

    Update training_algorithm.py

--- prosjekt_2_utlevert_kode/training_algorithm.py
+++ prosjekt_2_utlevert_kode/training_algorithm.py
@@ -29,7 +29,7 @@ def TrainingAlgorithm(problem):
         num_ints = 9 #M = eks heltall mellom 0 og 9, mulige siffre
 
         #henter treningsdata - kan ta inn argumenter her avhengig av 
-        data = get_train_test_sorting(length, num_ints, samples_per_batch = 250,n_batches_train = 10)
+        data = get_train_test_sorting(length, num_ints, samples_per_batch = 250,n_batches_train = 10, n_batches_test = 1)
         n_max = length
 
     x = data['x_train'][0]
